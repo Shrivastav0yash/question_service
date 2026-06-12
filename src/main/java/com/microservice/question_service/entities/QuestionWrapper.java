ac589066f90cad8c218773382c9f287a0e6a5efd
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-06-13 02:18:08 +0530

    Question service completed

--- src/main/java/com/microservice/question_service/entities/QuestionWrapper.java
+++ src/main/java/com/microservice/question_service/entities/QuestionWrapper.java
@@ -1,13 +1,10 @@
 package com.microservice.question_service.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @RequiredArgsConstructor
 public class QuestionWrapper {
 
